b77b0777ca0b6ab19500c377bc5aa1be35a4494b
ArnAfOslo <arnemg@gmail.com>
2019-06-06 08:53:04 +0200

    JSON og Liste fungerer

--- controllers/camsController.js
+++ controllers/camsController.js
@@ -3,9 +3,9 @@ var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
-var data = [{kameranavn: "PiCam", lokasjon: "Sofienbergparken Øst", aktiv: "Nei"}, 
-            {kameranavn: "Usb 30fps", lokasjon: "Slurpen 2. etg mot lekeplass", aktiv: "Ja"}, 
-            {kameranavn: "Nikon FM", lokasjon: "Botanisk Hage NordVestlig", aktiv: "Nei"} ];
+var data = [{kameranavn: "PiCam", lokasjon: "Sofienbergparken Øst", aktiv: "false"}, 
+            {kameranavn: "Usb 30fps", lokasjon: "Slurpen 2. etg mot lekeplass", aktiv: "true"}, 
+            {kameranavn: "Nikon FM", lokasjon: "Botanisk Hage NordVestlig", aktiv: "false"} ];
 
 // ---------------------------------------------------------------GET /
 app.get('/', function(req, res){
@@ -14,12 +14,22 @@ app.get('/', function(req, res){
 });
 
 
-// ---------------------------------------------------------------GET /slurpen
-app.get('/slurpen', function(req, res){
-  console.log("Er inne i GET /slurpen");
+// ---------------------------------------------------------------GET /regCam
+app.get('/regCam', function(req, res){
+  console.log("Er inne i GET /regCam");
   //kameras overføres til EJS filen og benyttes til å liste opp og hente ut data
-  res.render('slurpen', {kameras: data});
+  res.render('regCam', {kameras: data});
 });
+
+
+// ---------------------------------------------------------------POST /regCam
+app.post('/regCam', urlEncodedParser, function(req, res){
+  data.push(req.body);
+  //res.json({kamera: data});
+  res.render('cams', {kameras: data});
+  console.log("Er i POST /cams req.body --> " + JSON.stringify(req.body));
+});
+
 
 // ---------------------------------------------------------------GET /livevideo
 app.get('/livevideo', function(req, res){
@@ -38,21 +48,15 @@ app.get('/cams', function(req, res){
 
 // ---------------------------------------------------------------DELETE /cams
 app.delete('/cams/:kameranavn', function(req, res){
-
+  console.log("inne i DELETE param " + req.params.kameranavn);
   data = data.filter(function(kamera){
     return kamera.kameranavn.replace(/ /g, '-') !== req.params.kameranavn;
   });
-  res.json({kamera: data});
-  console.log("inne i DELETE param " + req.params.kameranavn);
+  res.render('cams', {kameras: data});
+  
 
 });
 
-// ---------------------------------------------------------------POST /cams
-app.post('/cams', urlEncodedParser, function(req, res){
-    data.push(req.body);
-    res.json({kamera: data});
-    //console.log("Er i POST /cams req.body --> " + req.body);
-  });
 
 
 
